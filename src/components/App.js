300c692289b4abaf2edb4594f81f7c1247c3c8cc
Abhishek verma <avishekv444@gmail.com>
2021-10-01 01:03:41 +0530

    Folder structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Tooltip from './tooltip';
+
+export default class App extends Component{
+ render(){
+   return(
+     <div>
+       <div>
+          <h1>Welcome to Tooltip Component</h1>
+       </div>
+
+     </div>
+   )
+ }
+}
