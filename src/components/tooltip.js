a587812457bf258a1653dfa9d424e5574738e5a7
Abhishek verma <avishekv444@gmail.com>
2021-10-01 02:07:38 +0530

    functionality completed

--- src/components/tooltip.js
+++ src/components/tooltip.js
@@ -1,15 +1,42 @@
 import React,{ Component }  from 'react';
+import '../stylesheet/tooltip.css'
 
 export default class tooltip extends Component{
 
     constructor(props){
         super(props);
+
+        this.state={
+            hovered:false
+        };
+    }
+
+    onHover=()=>{
+        this.setState({
+            hovered:true
+        })
+    }
+
+    onLeave=()=>{
+        this.setState({
+            hovered:false
+        })
     }
 
     render(){
+        
         const position=this.props.position;
+        const { hovered }=this.state;
+        console.log(hovered)
         return(
-            <h1>this is {position}</h1>
+            <div className="color half" >
+                <div onMouseEnter={this.onHover} onMouseLeave={this.onLeave}>
+                    <h1>this is {position}</h1>
+                    <span style={{visibility: hovered ? 'visible' : 'hidden'}}>
+                        <p>Thanks for Hovering! i am a tooltip</p>
+                    </span>
+                </div>
+            </div>
         )
     }
 }
